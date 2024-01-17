aec7cd0de03ed13464162ccf28120e582220f9fa
Masamune903 <masamune_903@outlook.jp>
2024-01-17 15:11:58 +0900

    next to this month

--- client/calendar/script.ts
+++ client/calendar/script.ts
@@ -5,12 +5,7 @@ calendar.attach(document);
 
 const today = new Date();
 
-const nextMonth = new Date(
-    new Date(today).setMonth(today.getMonth() + 1)
-);
-nextMonth.setDate(1);
-
 calendar.updateDisplay(CalendarDisplayOptions.from({
-    year: nextMonth.getFullYear(),
-    month: nextMonth.getMonth() + 1
+    year: today.getFullYear(),
+    month: today.getMonth() + 1
 }));
