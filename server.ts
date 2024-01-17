0c67557989e3a6e1070aaffead461c475b863c9a
Masamune903 <masamune_903@outlook.jp>
2024-01-17 15:08:53 +0900

    fix

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-import { Server } from "./serer/Server.ts";
-import { Debugger } from "./modules/util/Debugger/Debugger.ts";
+import { Server } from "./server/Server.ts";
+import { Debugger } from "@/modules/util/Debugger/Debugger.ts";
 
 new Server({ debugger: new Debugger(true) }).serve();
